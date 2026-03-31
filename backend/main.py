f90fac6006c77db9dd89ee0a7a585ad18bc8f9e3
Graceson <gwheatley@ftstudent.org>
2026-03-31 19:31:08 -0400

    simple frontend test

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
 import fetch_api
 
 app = FastAPI()
 
-
+app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"])
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
